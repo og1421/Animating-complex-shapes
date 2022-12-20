426741b953cc83ae0a750e10dddb5e1b416e6727
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:51:18 -0300

    Initial Commit

---
+++ Animating complex shapes/Animating_complex_shapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_complex_shapesApp.swift
+//  Animating complex shapes
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Animating_complex_shapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
