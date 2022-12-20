b0d1be42ae294fb2e3728a9ee5c431ca03f390e8
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 18:54:48 -0300

    finished

--- Animating complex shapes/ContentView.swift
+++ Animating complex shapes/ContentView.swift
@@ -7,17 +7,57 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Checkboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double>{
+        get{
+            AnimatablePair(Double(rows), Double(columns))
         }
-        .padding()
+        
+        set{
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        for row in 0..<rows{
+            for column in 0..<columns{
+                if (row + column).isMultiple(of: 2){
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
     }
 }
+
+struct ContentView: View {
+    @State private var rows = 4
+    @State private var columns = 4
+    
+    var body: some View {
+        Checkboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)){
+                    rows = 8
+                    columns = 8
+                }
+            }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
